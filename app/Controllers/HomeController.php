61c4b7999f5a62118391f7d3096a7c6d6e81be4e
timtiph <tiphany.moine@outlook.com>
2022-12-08 11:26:40 +0100

    Includes Header+nav

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,11 +6,21 @@ namespace App\Controllers;
 
 
 use Tmoi\Foundation\AbstractController;
+use Tmoi\Foundation\Authentication as Auth;
+use Tmoi\Foundation\View;
+
+;
 
 class HomeController extends AbstractController
 {
     public function index(): void
     {
-        
+        if (!Auth::check()) {
+            $this->redirect('login.form');
+        }
+        $user = Auth::get();
+        View::render('home', [
+            'user' => $user,
+        ]);
     }
 }
